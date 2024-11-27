e17643afdc0371b86fdb4c83bec3e00636ea4b31
FrancescoL96 <francesco29.fl@gmail.com>
2024-11-27 16:14:31 +0100

    fix: removed wrong defaulter

--- api/v1/types.go
+++ api/v1/types.go
@@ -116,7 +116,6 @@ type FocusSpec struct {
 	// +optional
 	CapacityReservationId string `yaml:"capacityReservationId" json:"capacityReservationId"`
 	// +kubebuilder:validation:Pattern=`(\b[Uu]used\b)|(\b[Uu]nused\b)`
-	// +kubebuilder:default=false
 	CapacityReservationStatus bool `yaml:"capacityReservationStatus" json:"capacityReservationStatus"`
 	// +kubebuilder:validation:Pattern=`(\b[Aa]djustment\b)|(\b[Pp]urchase\b)|(\b[Tt]ax\b)|(\b[Uu]sage\b)`
 	ChargeCategory string `yaml:"chargeCategory" json:"chargeCategory"`
